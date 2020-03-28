a6d7832939fe3bacf250e505d3a6844ef34704bc
evgeniy.goncharov <jtgn@yandex.ru>
2020-03-29 00:47:25 +0300

    добавили зависимости

---
+++ app/src/main/java/com/evgeny/goncharov/catapp/App.kt
@@ -0,0 +1,13 @@
+package com.evgeny.goncharov.catapp
+
+import android.app.Application
+
+class App : Application() {
+
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+}
---
+++ app/src/main/java/com/evgeny/goncharov/catapp/consts/Consts.kt
@@ -0,0 +1,3 @@
+package com.evgeny.goncharov.catapp.consts
+
+const val API_KEY = "93dbaff6-3db6-4ca8-bca8-cd03c57aed23"
