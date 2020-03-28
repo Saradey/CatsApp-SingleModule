c38a42504a35b89168bf397123d40c45d12dda3a
evgeniy.goncharov <jtgn@yandex.ru>
2020-03-28 23:59:17 +0300

    init commit

---
+++ app/src/main/java/com/evgeny/goncharov/catapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.evgeny.goncharov.catapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
